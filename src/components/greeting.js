bb5af516665c7def6c13d5835f6bb40ade4fdbe3
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2023-01-05 12:22:02 -0300

    greetings update

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Worlds(props) {
   return (
     <div>
-      {props.map((id, title, body) => (
+      {props.worlds.map((id, title, body) => (
         <div key={id}>
           <p>{title}</p>
           <h2>{body}</h2>
